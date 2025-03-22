9dc31663b471c3876204627b2c8722365332bc4a
mattl <mattl@LAPTOP-E70R6J6J>
2025-03-22 19:32:03 +0100

    Phase 2bis : Optimisation sur récup de select

---
+++ habilitations2024 Phase 2bis Optimisation sur récup de select/habilitations2024/dal/ProfilAccess.cs
@@ -0,0 +1,66 @@
+﻿using habilitations2024.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace habilitations2024.dal
+{
+    /// <summary>
+    /// Classe permettant de gérer les demandes concernant les profils
+    /// </summary>
+    public class ProfilAccess
+    {
+        /// <summary>
+        /// Instance unique de l'accès aux données
+        /// </summary>
+        private readonly Access access = null;
+
+        /// <summary>
+        /// Constructeur pour créer l'accès aux données
+        /// </summary>
+        public ProfilAccess()
+        {
+            access = Access.GetInstance();
+        }
+
+        /// <summary>
+        /// Récupère et retourne les profils
+        /// </summary>
+        /// <returns>liste des profils</returns>
+        public List<Profil> GetLesProfils()
+        {
+            List<Profil> lesProfils = new List<Profil>();
+            if (access.Manager != null)
+            {
+                string req = "select * from profil order by nom;";
+                try
+                {
+                    List<Object[]> records = access.Manager.ReqSelect(req);
+                    if (records != null)
+                    {
+                        lesProfils = records.ConvertAll(new Converter<Object[], Profil>(ConvertProfil));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Environment.Exit(0);
+                }
+            }
+            return lesProfils;
+        }
+
+        /// <summary>
+        /// Utilise les valeurs d'un tableau pour créer un objet de type Profil
+        /// </summary>
+        /// <param name="profil">tableau d'objets contenant les valeurs des champs du profil</param>
+        /// <returns>objet de type Profil</returns>
+        private Profil ConvertProfil(Object[] profil)
+        {
+            return new Profil((int)profil[0], (string)profil[1]);
+        }
+
+    }
+}
